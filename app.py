7a1742e0209ade48cca53d94c24592cf0968a1ba
iwilldocoding <shubham@biobrain.io>
2026-02-18 15:58:08 +0530

    another change

--- app.py
+++ app.py
@@ -97,6 +97,22 @@ app=FastAPI()
 async def create_items(item_id:int,item:Item):
     return {"item_id":item_id,**item.model_dump()}
 '''
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name : str 
+    description : str | None=None
+    price : float
+    tax : float | None = None
+
+app = FastAPI()
+@app.put("/items/{item_id}")
+async def update_item(item_id:int, item:Item, q:str|None=None):
+    result={"item_id":item_id,**item.model_dump()}
+    if q:
+        result.update({"q":q})
+    return result#here we are updating the result dictonary during the execution for a given conition 
 
 
 
